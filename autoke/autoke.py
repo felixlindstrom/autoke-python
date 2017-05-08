04eda9723ff19eea8f0fd13bf56ec88010632157
Felix Lindström <felix.lindstrom@mediaplanet.com>
2017-05-08 10:43:31 +0200

    Added missing docstrings

--- autoke/autoke.py
+++ autoke/autoke.py
@@ -4,11 +4,25 @@ from collections import defaultdict
 
 
 class Engine:
+    """
+    Keyword Extractor Engine
+    
+    Args:
+        stop_list (StopList): StopList instance to use
+    """
     def __init__(self, stop_list):
         self.stop_list = stop_list
 
     def get_phrases(self, text):
-        """Create 'phrases' by splitting a sentence by stop-words"""
+        """
+        Extract 'phrases' from text by splitting a sentence by stop-words.
+        
+        Args:
+            text (string): Text to extract phrases from
+            
+        Returns:
+            dict of phrases
+        """
         sentences = util.get_sentences(text)
         phrases = []
         for sentence in sentences:
@@ -16,7 +30,15 @@ class Engine:
         return phrases
 
     def score_words(self, phrases):
-        """Score words across multiple phrases"""
+        """
+        Score words across multiple phrases
+        
+        Args:
+            phrases (dict): Phrases to score
+            
+        Returns:
+            dict of scored words
+        """
         frequencies = defaultdict(float)
         degrees = defaultdict(float)
         scores = defaultdict(float)
@@ -33,7 +55,15 @@ class Engine:
         return dict(scores)
 
     def score_phrases(self, phrases):
-        """Score phrases based on word-scores"""
+        """
+        Score phrases based on word-scores
+        
+        Args:
+            phrases (dict): Phrases to score
+            
+        Returns:
+            dict of scored phrases
+        """
         word_scores = self.score_words(phrases)
         scores = defaultdict(float)
         for phrase in phrases:
@@ -42,6 +72,16 @@ class Engine:
         return dict(scores)
 
     def analyse(self, text, min_score=0):
+        """
+        Analyse a text, generating scored phrases
+        
+        Args:
+             text (string): Text to analyse
+             min_score (integer): Minimum score of result-items to include
+             
+        Returns:
+            Dictionary of phrases and their scores
+        """
         phrases = self.get_phrases(text)
         scores = self.score_phrases(phrases)
 
@@ -52,12 +92,24 @@ class Engine:
 
 
 class StopList:
+    """
+    Stop-list
+    
+    Args:
+        file_path (string) (optional): File-path to load
+    """
     def __init__(self, file_path=None):
         self.words = []
         if file_path is not None:
             self.load_file(file_path)
 
     def load_file(self, file_path):
+        """
+        Load a file of stop-list and append to the list of words
+        
+        Args:
+            file_path (string): Path to the file to load
+        """
         with open(file_path) as fh:
             for line in fh.readlines():
                 if not line.strip():
@@ -65,6 +117,12 @@ class StopList:
                 self.add(line)
 
     def get_compiled_pattern(self):
+        """
+        Get compiled regex for the words
+        
+        Returns:
+            retype
+        """
         if len(self.words) < 1:
             raise ValueError('No words loaded')
 
@@ -72,11 +130,35 @@ class StopList:
         return re.compile('|'.join(words), re.IGNORECASE)
 
     def add(self, word):
+        """
+        Add a word to the list of stop-words
+        
+        Args:
+            word (string): Word to add
+        """
         self.words.append(str(word).lower().strip())
 
     def has(self, word):
+        """
+        Check if a word is included in the current instance
+        
+        Args:
+            word (string): Word to check for
+            
+        Returns:
+            True if word exists, otherwise False
+        """
         return str(word).strip().lower() in self.words
 
     def split(self, string):
+        """
+        Split a given string by stop-words
+        
+        Args:
+            string (string): String to split
+            
+        Returns:
+            A list of strings
+        """
         result = re.sub(self.get_compiled_pattern(), '|', string).split('|')
         return util.clean_list(result)
--- autoke/util.py
+++ autoke/util.py
@@ -2,19 +2,43 @@ import re
 
 
 def clean_list(lst):
-    """Clean a list by removing empty entries, casting them to strings and lowering the case"""
+    """
+    Clean a list by removing empty entries, casting them to strings and lowering the case
+    
+    Args:
+        lst (list): List to clean
+        
+    Returns:
+        A cleaned version on the inputted list
+    """
     lst = list(map(lambda item: str(item).strip().lower(), lst))
     lst = list(filter(lambda item: item, lst))
     return lst
 
 
 def get_words(string):
-    """Split a string into words"""
+    """
+    Split a string into words
+    
+    Args:
+        string (string): String to split into words
+        
+    Return:
+        list with words
+    """
     words = re.compile("[^a-zA-Z]").split(string)
     return clean_list(words)
 
 
 def get_sentences(string):
-    """Get sentences, by punctuation"""
+    """
+    Get sentences, by punctuation
+    
+    Args:
+        string (string): String to split into sentences
+        
+    Returns:
+        list with sentences
+    """
     sentences = re.compile("[.!?,;:']").split(string)
     return clean_list(sentences)
